581ed15d534288fa95a5de7921e87cd99aa6f42e
Ben Myers <benl.myers01@gmail.com>
2021-05-05 18:43:28 -0700

    🧮 Added Price Manager

--- src/main/java/com/characterlim/exportsplugin/manager/PriceManager.java
+++ src/main/java/com/characterlim/exportsplugin/manager/PriceManager.java
@@ -1,6 +1,40 @@
 package com.characterlim.exportsplugin.manager;
 
+import com.characterlim.exportsplugin.config.ConfigManager;
+
+import java.util.List;
+
 public class PriceManager {
 
+    public static int getSellPrice(String material) {
+        List<String> items = ConfigManager.getExportItems();
+        List<Integer> counts = ConfigManager.getExportCounts();
+        int i = items.indexOf(material);
+        int n = counts.get(i);
+        int m = items.size();
+        int base = ConfigManager.getBasePrice();
+        int startPrice = base * m;
+        int totalExports = totalOf(counts);
+        double p = n / totalExports;
+        double price = startPrice / p;
+        return (int) Math.ceil(price);
+    }
 
+    public static void addCount(String material) {
+        List<String> items = ConfigManager.getExportItems();
+        List<Integer> counts = ConfigManager.getExportCounts();
+        int i = items.indexOf(material);
+        int n = counts.get(i);
+        n += 1;
+        counts.set(i, n);
+        ConfigManager.setExportCounts(counts);
+    }
+
+    private static int totalOf(List<Integer> list) {
+        int count = 0;
+        for(Integer val: list) {
+            count += val;
+        }
+        return count;
+    }
 }
